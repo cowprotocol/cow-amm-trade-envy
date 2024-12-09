2324f5feb4edcb007c64c7866af0d6960faafdde
Thomas Niedermayer <thomas.n71@gmail.com>
2024-12-09 18:14:17 +0100

    Refactor missed_surplus.py

--- missed_surplus.py
+++ missed_surplus.py
@@ -4,42 +4,44 @@ from web3 import Web3
 from dotenv import load_dotenv
 
 load_dotenv()
+
 node_url = os.getenv("NODE_URL")
 
-# topics are hexbytes
-def isin_bondingpool(topic: bytes, pool_address: str) -> bool:
-    return pool_address.lower() in topic.hex()
+def analyze_surplus(file_path, node_url, pool_address):
+    w3 = Web3(Web3.HTTPProvider(node_url, request_kwargs={'timeout': 60}))
+    pool_address = pool_address.lower()
 
-def log_isused(log, pool_address: str) -> bool:
-    return any([isin_bondingpool(topic, pool_address) for topic in log.topics])
+    def isin_pool(topic):
+        return pool_address in topic.hex()
 
-def logs_isused(logs, pool_address: str) -> bool:
-    return any([log_isused(log, pool_address) for log in logs])
+    def log_is_used(log):
+        return any(isin_pool(topic) for topic in log.topics)
 
-if __name__ == '__main__':
-    filepath = 'cow_amm_missed_surplus.csv'
+    def logs_are_used(logs):
+        return any(log_is_used(log) for log in logs)
 
-    weth_usdc_pool_addr = "f08d4dea369c456d26a3168ff0024b904f2d8b91"
+    file = pd.read_csv(file_path)
+    total_surplus, used_surplus = 0, 0
 
-    file = pd.read_csv(filepath)
-    w3 = Web3(Web3.HTTPProvider(
-        node_url,
-        request_kwargs={'timeout': 60}
-    ))
-    print(f"Potential surplus: {file['potential_surplus'][file['potential_surplus'] > 0].sum()}")
-    print(f"Block range: {file['call_block_number'].min()} - {file['call_block_number'].max()}")
-
-    total_surplus = 0
-    used_surplus = 0
     for _, row in file.iterrows():
-        if row['potential_surplus'] and row['potential_surplus'] > 0:
-            txs = w3.eth.get_transaction_receipt(row['call_tx_hash'])
-            isUsed = logs_isused(txs.logs, weth_usdc_pool_addr)
-            if not isUsed:
+        if row['potential_surplus'] > 0:
+            tx_receipt = w3.eth.get_transaction_receipt(row['call_tx_hash'])
+            is_used = logs_are_used(tx_receipt.logs)
+
+            if not is_used:
                 total_surplus += row['potential_surplus']
             else:
                 used_surplus += row['potential_surplus']
 
-    print(f"Total unused surplus: {total_surplus/10**18}")
-    print(f"Total used surplus: {used_surplus/10**18}")
-    
+    return {
+        "total_unused_surplus": total_surplus / 10**18,
+        "total_used_surplus": used_surplus / 10**18,
+    }
+
+if __name__ == '__main__':
+    file_path = 'cow_amm_missed_surplus.csv'
+    pool_address = "f08d4dea369c456d26a3168ff0024b904f2d8b91"
+
+    surplus_results = analyze_surplus(file_path, node_url, pool_address)
+    print(f"Total unused surplus: {surplus_results['total_unused_surplus']}")
+    print(f"Total used surplus: {surplus_results['total_used_surplus']}")
