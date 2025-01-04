8057c66da29245ff31e75ed297b559b8ecfb5347
Thomas Niedermayer <thomas.n71@gmail.com>
2025-01-04 12:48:53 +0100

    remove dagster

--- Makefile
+++ Makefile
@@ -7,8 +7,5 @@ format:
 lint:
 	uv run ruff check
 
-dagster-run:
-	uv run dagster dev -w workspace.yaml
-
 run-pipeline:
 	uv run src/cow_amm_trade_envy/main.py
--- src/cow_amm_trade_envy/jobs/calculate_envy_job.py
+++
@@ -1,9 +0,0 @@
-from dagster import job
-from cow_amm_trade_envy.assets.assets import (
-    dune_settlement_data,
-)
-
-
-@job
-def envy_job():
-    run_dune_settlement_data = dune_settlement_data()
